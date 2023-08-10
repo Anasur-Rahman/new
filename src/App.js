03423cd0d95cc1799c914d83fd0d2fef7a780761
Anasur Rahman <87699410+Anasur-Rahman@users.noreply.github.com>
2023-08-10 21:54:39 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {data} from './data/data.js'; 
+import StudentTable from './Component/StudentTable.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/index.css';
+import CollegeChanceCalculator from './Component/CollegeChanceCalculator.js';
+
+
+
+const App = () => {
+  return (
+    <div className='main'>
+
+      <div className='header-text'>
+        <h1 className='h1'>Calculate Your Chance To get into A GOVT. College</h1>
+        <h2 className='h2'>This is valid only for Science Group</h2>
+      </div>
+
+      <CollegeChanceCalculator studentData={data} />
+      <a className='anchor' href="#bottom">How This App Calculate Your Chance</a>
+      <StudentTable data={data} itemsPerPage={100} />
+      <p id='bottom'>Your chance to get into a above college is estimated based on your total mark and your position in CTG Board.It includes the total number of seats having by the College.This app also considers the student who may migrate to other districts .This chance may vary as Total Marks of a student can change after Recheck Result. </p>
+      
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+export default App;
---
+++ src/Component/StudentTable.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import { Table, InputGroup, FormControl, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../css/StudentTable.css";
+//import Navbar from '../Component/Navbar';
+
+const StudentTable = ({ data, itemsPerPage }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredData, setFilteredData] = useState(data);
+
+  useEffect(() => {
+    const filtered = data.filter(
+      (student) =>
+        student.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        student.roll.toString().includes(searchQuery)
+    );
+    setFilteredData(filtered);
+  }, [data, searchQuery]);
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+    setCurrentPage(1); // Reset pagination when the search query changes
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  // Calculate total pages based on the number of items per page
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+
+  // Calculate the index range for the current page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  return (
+    <div className="container mt-4">
+      <h1 className='display-4'>Board STAND</h1>
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Search by name or roll"
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+      </InputGroup>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Roll</th>
+            <th>GPA</th>
+            <th>Number</th>
+            <th>Rank</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData.slice(startIndex, endIndex).map((student, index) => (
+            <tr key={index}>
+              <td>{student.name}</td>
+              <td>{student.roll}</td>
+              <td>{student.gpa}</td>
+              <td>{student.total_number}</td>
+              <td>{student.position}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <div className="d-flex justify-content-center mt-4">
+        <Button
+          variant="primary"
+          disabled={currentPage === 1}
+          onClick={handlePrevPage}
+          className="mr-2"
+        >
+          Previous
+        </Button>
+        <Button
+          variant="primary"
+          disabled={currentPage === totalPages}
+          onClick={handleNextPage}
+          className='mr-2'
+        >
+          Next
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default StudentTable;
